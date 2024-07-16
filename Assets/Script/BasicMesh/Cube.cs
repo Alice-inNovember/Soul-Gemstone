838d46da83c99cdb3d9578cd23f762ab82c4973f
AliceInNovember <alice@alicenov.com>
2024-07-16 09:35:33 +0900

    Shader Edit

--- Assets/Script/BasicMesh/Cube.cs
+++ Assets/Script/BasicMesh/Cube.cs
@@ -1,135 +1,144 @@
-﻿using UnityEngine;
+﻿using System;
+using Unity.Mathematics;
+using UnityEngine;
 
 namespace Script.BasicMesh
 {
     public class Cube : MonoBehaviour
     {
         [SerializeField] private Material material;
+        private Mesh _mesh;
+        private MeshRenderer _meshRenderer;
+        private MeshFilter _meshFilter;
+
+        private float _testSpace = 0;
 
         void Start()
         {
-            // 정점 정의
-            Vector3[] vertices = new Vector3[]
+            _mesh = new Mesh();
+            _meshRenderer = gameObject.AddComponent<MeshRenderer>();
+            _meshFilter = gameObject.AddComponent<MeshFilter>();
+            _meshRenderer.material = material;
+            _meshFilter.mesh = _mesh;
+
+            // Set initial mesh data
+            SetVertices();
+            SetTriangles();
+            SetUVs();
+            _mesh.RecalculateNormals();
+        }
+
+        private void Update()
         {
-            // Front face
-            new Vector3(-1, -1, 1), // 0
-            new Vector3(1, -1, 1),  // 1
-            new Vector3(1, 1, 1),   // 2
-            new Vector3(-1, 1, 1),  // 3
+            _testSpace = math.sin(Time.time) * 0.5f;
+            SetVertices();
+            _mesh.RecalculateNormals();
+        }
 
-            // Back face
-            new Vector3(-1, -1, -1), // 4
-            new Vector3(1, -1, -1),  // 5
-            new Vector3(1, 1, -1),   // 6
-            new Vector3(-1, 1, -1),  // 7
-
-            // Top face
-            new Vector3(-1, 1, 1),   // 8
-            new Vector3(1, 1, 1),    // 9
-            new Vector3(1, 1, -1),   // 10
-            new Vector3(-1, 1, -1),  // 11
-
-            // Bottom face
-            new Vector3(-1, -1, 1),  // 12
-            new Vector3(1, -1, 1),   // 13
-            new Vector3(1, -1, -1),  // 14
-            new Vector3(-1, -1, -1), // 15
-
-            // Left face
-            new Vector3(-1, -1, 1),  // 16
-            new Vector3(-1, 1, 1),   // 17
-            new Vector3(-1, 1, -1),  // 18
-            new Vector3(-1, -1, -1), // 19
-
-            // Right face
-            new Vector3(1, -1, 1),   // 20
-            new Vector3(1, 1, 1),    // 21
-            new Vector3(1, 1, -1),   // 22
-            new Vector3(1, -1, -1)   // 23
-        };
-
-        // 삼각형 정의 (정점의 인덱스, 반시계 방향)
-        int[] triangles = new int[]
+        private void SetVertices()
         {
-            // Front face
-            0, 1, 2,
-            0, 2, 3,
+            var vertices = new Vector3[]
+            {
+                // Front face
+                new(-1 + _testSpace, -1 + _testSpace, 1 + _testSpace), // 0
+                new(1, -1, 1), // 1
+                new(1, 1, 1), // 2
+                new(-1, 1, 1), // 3
 
-            // Back face
-            4, 6, 5,
-            4, 7, 6,
+                // Back face
+                new(-1, -1, -1), // 4
+                new(1, -1, -1), // 5
+                new(1, 1, -1), // 6
+                new(-1, 1, -1), // 7
 
-            // Top face
-            8, 9, 10,
-            8, 10, 11,
+                // Top face
+                new(-1, 1, 1), // 8
+                new(1, 1, 1), // 9
+                new(1, 1, -1), // 10
+                new(-1, 1, -1), // 11
 
-            // Bottom face
-            12, 14, 13,
-            12, 15, 14,
+                // Bottom face
+                new(-1 + _testSpace, -1 + _testSpace, 1 + _testSpace), // 12
+                new(1, -1, 1), // 13
+                new(1, -1, -1), // 14
+                new(-1, -1, -1), // 15
 
-            // Left face
-            16, 17, 18,
-            16, 18, 19,
+                // Left face
+                new(-1 + _testSpace, -1 + _testSpace, 1 + _testSpace), // 16
+                new(-1, 1, 1), // 17
+                new(-1, 1, -1), // 18
+                new(-1, -1, -1), // 19
 
-            // Right face
-            20, 22, 21,
-            20, 23, 22
-        };
+                // Right face
+                new(1, -1, 1), // 20
+                new(1, 1, 1), // 21
+                new(1, 1, -1), // 22
+                new(1, -1, -1) // 23
+            };
+            _mesh.vertices = vertices;
+        }
 
-        // UV 좌표 정의
-        Vector2[] uvs = new Vector2[]
+        private void SetTriangles()
         {
-            // Front face
-            new Vector2(0, 0),
-            new Vector2(1, 0),
-            new Vector2(1, 1),
-            new Vector2(0, 1),
+            var triangles = new[]
+            {
+                // Front face
+                0, 2, 1,
+                0, 3, 2,
+                // Back face
+                4, 5, 6,
+                4, 6, 7,
+                // Top face
+                8, 10, 9,
+                8, 11, 10,
+                // Bottom face
+                12, 13, 14,
+                12, 14, 15,
+                // Left face
+                16, 18, 17,
+                16, 19, 18,
+                // Right face
+                20, 21, 22,
+                20, 22, 23
+            };
+            _mesh.triangles = triangles;
+        }
 
-            // Back face
-            new Vector2(0, 0),
-            new Vector2(1, 0),
-            new Vector2(1, 1),
-            new Vector2(0, 1),
-
-            // Top face
-            new Vector2(0, 0),
-            new Vector2(1, 0),
-            new Vector2(1, 1),
-            new Vector2(0, 1),
-
-            // Bottom face
-            new Vector2(0, 0),
-            new Vector2(1, 0),
-            new Vector2(1, 1),
-            new Vector2(0, 1),
-
-            // Left face
-            new Vector2(0, 0),
-            new Vector2(1, 0),
-            new Vector2(1, 1),
-            new Vector2(0, 1),
-
-            // Right face
-            new Vector2(0, 0),
-            new Vector2(1, 0),
-            new Vector2(1, 1),
-            new Vector2(0, 1)
-        };
-
-        // Mesh 생성 및 할당
-        Mesh mesh = new Mesh();
-        mesh.vertices = vertices;
-        mesh.triangles = triangles;
-        mesh.uv = uvs;
-        mesh.RecalculateNormals();
-        mesh.vertices[0]
-        // MeshFilter 및 MeshRenderer 추가 및 설정
-        MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
-        MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
-        meshFilter.mesh = mesh;
-
-            // 기본 Material 할당 (필요시 Material을 변경할 수 있습니다)
-            meshRenderer.material = material;
+        private void SetUVs()
+        {
+            var uvs = new Vector2[]
+            {
+                new (0, 0),
+                new (1, 0),
+                new (1, 1),
+                new (0, 1),
+                
+                new (0, 0),
+                new (1, 0),
+                new (1, 1),
+                new (0, 1),
+                
+                new (0, 0),
+                new (1, 0),
+                new (1, 1),
+                new (0, 1),
+                
+                new (0, 0),
+                new (1, 0),
+                new (1, 1),
+                new (0, 1),
+                
+                new (0, 0),
+                new (1, 0),
+                new (1, 1),
+                new (0, 1),
+                
+                new (0, 0),
+                new (1, 0),
+                new (1, 1),
+                new (0, 1)
+            };
+            _mesh.uv = uvs;
         }
     }
 }
