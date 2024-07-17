5b05994b56e6d701936a68e1af92678b5dacaf57
AliceInNovember <alice@alicenov.com>
2024-07-18 01:07:32 +0900

    gem fin

--- Assets/Crystal Shader/GemShapeList.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "GemShapeList", menuName = "ScriptableObject/GemShapeList", order = int.MaxValue)]
-public class GemShapeList : ScriptableObject
-{
-	public List<GameObject> shapeList;
-}
